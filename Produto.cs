b71656be3aa0ffb01969ab21e4181abfdc48095b
Daniel Stephanio Nunes <dti584@localiza.com>
2023-02-28 11:07:35 -0300

    Add project files.

---
+++ Produto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_POO_02_Produto
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque() => Preco * Quantidade;
+
+        public void AdcionarProdutos(int quantidade) => Quantidade += quantidade;
+
+        public void RemoverProdutos(int quantidade) => Quantidade -= quantidade;
+
+        public override string ToString()
+        {
+            return Nome + ", " +
+                "R$: " +
+                Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " +
+                Quantidade +
+                " unidades, Total: R$" +
+                ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*Fazer um programa para ler os dados de um produto em estoque (nome, preço e
+quantidade no estoque). Em seguida:
+• Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no
+estoque)
+• Realizar uma entrada no estoque e mostrar novamente os dados do produto
+• Realizar uma saída no estoque e mostrar novamente os dados do produto
+Para resolver este problema, você deve criar
+uma CLASSE conforme projeto ao lado:
+Entre os dados do produto:
+Nome: TV
+Preço: 900.00
+Quantidade no estoque: 10
+Dados do produto: TV, $ 900.00, 10 unidades, Total: $ 9000.00
+Digite o número de produtos a ser adicionado ao estoque: 5
+Dados atualizados: TV, $ 900.00, 15 unidades, Total: $ 13500.00
+Digite o número de produtos a ser removido do estoque: 3
+Dados atualizados: TV, $ 900.00, 12 unidades, Total: $ 10800.00*/
+
+using Exercicio_POO_02_Produto;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o nome do produto: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Digite o preço do produto: ");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine("Digite a quantidade do produto: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Produto produto = new Produto(nome, preco, quantidade);
+
+        Console.WriteLine("Dados do produto: " + produto + "\n");
+
+        Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+        int adicionarQtd = int.Parse(Console.ReadLine());
+        produto.AdcionarProdutos(adicionarQtd);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto + "\n");
+
+        Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+        int subtrairQtd = int.Parse(Console.ReadLine());
+        produto.RemoverProdutos(subtrairQtd);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto);
+    }
+}
